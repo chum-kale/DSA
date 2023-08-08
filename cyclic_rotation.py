c50b0eca19166d9492ca82c10401b102f3ebfa49
Chinmay Kale <82819222+chum-kale@users.noreply.github.com>
2023-08-08 22:14:05 +0530

    Add files via upload

---
+++ cyclic_rotation.py
@@ -0,0 +1,30 @@
+def rotate( arr, n):
+    last = arr[-1]
+    arr.insert(0, last)
+    del arr[-1]
+    
+    return arr
+    
+    
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+def main():
+
+    T = int(input())
+
+    while(T > 0):
+        n = int(input())
+        a = [int(x) for x in input().strip().split()]
+        rotate(a, n)
+        print(*a)
+
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
